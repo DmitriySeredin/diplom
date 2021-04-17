ca0a7647b5943e326fdca37482625bdedc732eed
Даниил Никитин <faverus061999@gmail.com>
2021-04-17 17:41:52 +0300

    SignIn layout

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import SignUp from "./Components/LoginPage/SignUpPage/SignUp";
+import SignIn from "./Components/LoginPage/SignInPage/SignIn";
 import MainPage from "./Components/MainPage/MainPage";
 
 const App = () => {
@@ -9,6 +10,7 @@ const App = () => {
       <Switch>
         <Route exact path="/" render={() => <MainPage />} />
         <Route path="/signup" render={() => <SignUp />} />
+        <Route path="/signin" render={() => <SignIn />} />
       </Switch>
     </BrowserRouter>
   );
---
+++ src/Components/LoginPage/SignInPage/SignIn.jsx
@@ -0,0 +1,52 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+import style from "./SignIn.module.css";
+
+const SignIn = () => {
+  return (
+    <div className={style.signIn}>
+      <h1>HARAKTER</h1>
+      <div className={style.formWrapper}>
+        <div className={style.links}>
+          <div className={style.linkWrap}>
+            <span className={style.link}>
+              <NavLink to="/signup" className={style.a}>
+                Регистрация
+              </NavLink>
+            </span>
+          </div>
+          <div className={style.linkWrap}>
+            <span className={style.link}>
+              <NavLink to="/signin" className={style.a}>
+                Войти
+              </NavLink>
+            </span>
+          </div>
+        </div>
+        <form className={style.form}>
+          <label htmlFor="email">
+            АДРЕС ЭЛЕКТРОННОЙ ПОЧТЫ:
+            <input type="email" name="email" />
+          </label>
+          <label htmlFor="password">
+            ПАРОЛЬ:
+            <input type="password" name="password" />
+          </label>
+          <button className={style.btn}>ВОЙТИ</button>
+        </form>
+        <div className={style.googleAuth}>
+          <h3>ИЛИ ВОЙДИ С ПОМОЩЬЮ...</h3>
+          <button className={style.googlebtn}>
+            <img
+              src="https://www.freepnglogos.com/uploads/google-logo-png/google-logo-png-suite-everything-you-need-know-about-google-newest-0.png"
+              alt=""
+            />
+            <strong>GOOGLE</strong>
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default SignIn;
